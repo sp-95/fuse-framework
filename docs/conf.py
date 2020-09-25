4eca1dd78b7f54568bab8385933e9f74312be469
Shashanka Prajapati <shashanka@fusemachines.com>
2020-09-25 17:50:30 +0545

    refactor: Replace tomlkit with toml to read package meta for docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,52 +13,21 @@
 # import os
 # import sys
 
-import sphinx
-import tomlkit
+import toml
 
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Monkeypatches -----------------------------------------------------------
-
-def _monkeypatch(cls):
-    """Decorator to monkey-patch methods."""
-
-    def decorator(func):
-        method = func.__name__
-        old = getattr(cls, method)
-        setattr(
-            cls,
-            method,
-            lambda inst, *args, **kwargs: func(inst, old, *args, **kwargs),
-        )
-
-    return decorator
-
-
-# workaround until https://github.com/miyakogi/m2r/pull/55 is merged
-@_monkeypatch(sphinx.registry.SphinxComponentRegistry)
-def add_source_parser(self, _old_add_source_parser, *args, **kwargs):
-    """This function changed in sphinx v3.0, we need to fix it back."""
-    # signature is (parser: Type[Parser], **kwargs), but m2r expects
-    # the removed (str, parser: Type[Parser], **kwargs).
-    if isinstance(args[0], str):
-        args = args[1:]
-    return _old_add_source_parser(self, *args, **kwargs)
+# sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
 def _get_project_meta():
-    with open("../pyproject.toml") as pyproject:
-        file_contents = pyproject.read()
-
-    return tomlkit.parse(file_contents)["tool"]["poetry"]
+    return toml.load("../pyproject.toml")["tool"]["poetry"]
 
 
 pkg_meta = _get_project_meta()
-copyright = '2020, Shashanka Prajapati'
-author = 'Shashanka Prajapati'
+project = str(pkg_meta["name"])
+copyright = "2020, Shashanka Prajapati"
+authors = str(pkg_meta["authors"])
 
 # The full version, including alpha/beta/rc tags
 release = str(pkg_meta["version"])
@@ -66,7 +35,7 @@ release = str(pkg_meta["version"])
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
     "sphinx.ext.coverage",
@@ -78,24 +47,24 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = [".rst", ".md"]
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # -- Options for todo extension ----------------------------------------------
 
@@ -107,7 +76,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
