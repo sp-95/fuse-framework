6748e1740d2a3ab91b370cfd4059d44a67be0a56
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-08 22:57:36 +0545

    chore: Use fire to fire the command

--- fuse_framework/main.py
+++ fuse_framework/main.py
@@ -2,39 +2,56 @@ import os
 import subprocess
 from distutils import spawn
 
+import fire
+import toml
 from cookiecutter import exceptions
 from cookiecutter.main import cookiecutter
 from loguru import logger
 
 
-def init():
-    if not spawn.find_executable("git"):
-        logger.warning("git not installed in your system")
-        logger.info("Downloading git")
-        subprocess.run(["sudo", "apt", "install", "git"])
+class Main:
+    def __init__(self):
+        self.version = self._get_project_meta()["version"]
 
-    if not spawn.find_executable("python3"):
-        logger.warning("python3 not installed in your system")
-        logger.info("Downloading python3")
-        subprocess.run(["sudo", "apt", "install", "python3"])
+    @staticmethod
+    def _get_project_meta():
+        return toml.load("pyproject.toml")["tool"]["poetry"]
 
-    if not spawn.find_executable("poetry"):
-        logger.warning("poetry not installed")
-        logger.info("Installing poetry")
-        subprocess.run(["pip3", "install", "poetry", "--user", "--upgrade", "--pre"])
+    @staticmethod
+    def init():
+        if not spawn.find_executable("git"):
+            logger.warning("git not installed in your system")
+            logger.info("Downloading git")
+            subprocess.run(["sudo", "apt", "install", "git"])
 
-    while True:
-        try:
-            path = cookiecutter("gh:sp-fm/fuse-framework")
-            os.chdir(path)
-            subprocess.run(["git", "init"])
-            subprocess.run(["poetry", "install"])
-            subprocess.run(["poetry", "run", "pre-commit", "install"])
-            break
-        except exceptions.RepositoryNotFound:
-            logger.error("Download incomplete!")
-        except exceptions.FailedHookException:
-            logger.error("Please enter valid inputs")
-        except exceptions.OutputDirExistsException as e:
-            logger.error(str(e))
-            break
+        if not spawn.find_executable("python3"):
+            logger.warning("python3 not installed in your system")
+            logger.info("Downloading python3")
+            subprocess.run(["sudo", "apt", "install", "python3"])
+
+        if not spawn.find_executable("poetry"):
+            logger.warning("poetry not installed")
+            logger.info("Installing poetry")
+            subprocess.run(
+                ["pip3", "install", "poetry", "--user", "--upgrade", "--pre"],
+            )
+
+        while True:
+            try:
+                path = cookiecutter("gh:sp-fm/fuse-framework")
+                os.chdir(path)
+                subprocess.run(["git", "init"])
+                subprocess.run(["poetry", "install"])
+                subprocess.run(["poetry", "run", "pre-commit", "install"])
+                break
+            except exceptions.RepositoryNotFound:
+                logger.error("Download incomplete!")
+            except exceptions.FailedHookException:
+                logger.error("Please enter valid inputs")
+            except exceptions.OutputDirExistsException as e:
+                logger.error(str(e))
+                break
+
+
+def main():
+    fire.Fire(Main)
